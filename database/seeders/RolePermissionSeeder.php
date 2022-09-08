5307bad647cb939501051cda8580d55e9ddf37c1
Zakir Hossen <web.zakirbd@gmail.com>
2022-09-08 14:39:04 +0600

    feat: spatie roles and permission added

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create(['name' => 'admin']);
+
+        $permissions = [
+            ['name' => 'user list'],
+            ['name' => 'create user'],
+            ['name' => 'edit user'],
+            ['name' => 'delete user'],
+            ['name' => 'role list'],
+            ['name' => 'create role'],
+            ['name' => 'edit role'],
+            ['name' => 'delete role'],
+        ];
+
+        foreach ($permissions as $item) {
+            Permission::create($item);
+        }
+
+        $role->syncPermissions(Permission::all());
+
+        $user = User::first();
+        $user->assignRole($role);
+    }
+}
